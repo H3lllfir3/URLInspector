8ac156eecab47705bbe077d1ec68a56a4bc8d006
H3lllfir3 <gholami.ayub73+github@gmail.com>
2023-07-13 23:43:54 +0200

    script to create table.

---
+++ database.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+
+def create_table():
+    conn = sqlite3.connect('data.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS url_data (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    url TEXT,
+                    title TEXT,
+                    status_code INTEGER,
+                    body TEXT,
+                    js_hash TEXT,
+                    js_file TEXT,
+                    content_length INTEGER,
+                    added_time TEXT
+                )''')
+    conn.commit()
+    conn.close()
+
+create_table()
