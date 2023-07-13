299befbfa40e170acee846f9ca555e697a773d58
H3lllfir3 <gholami.ayub73+github@gmail.com>
2023-07-13 23:42:01 +0200

    bot for send notification using discord.

---
+++ bot.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+
+class DiscordWebhook:
+    def __init__(self, webhook_url):
+        self.webhook_url = webhook_url
+        self.headers = {
+            "Content-Type": "application/json"
+        }
+
+    def send_message(self, content):
+        payload = {
+            "content": content
+        }
+
+        response = requests.post(self.webhook_url, data=json.dumps(payload), headers=self.headers)
+
+        if response.status_code == 204:
+            print("Message sent successfully.")
+        else:
+            print(f"Failed to send message. Status code: {response.status_code}")
+            print(response.text)
