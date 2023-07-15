daf3d5d3eb0caa2e1bf745a40447918865cdaa0c
H3lllfir3 <gholami.ayub73+github@gmail.com>
2023-07-15 23:52:25 +0200

    fixed type.

--- cli.py
+++ cli.py
@@ -1,7 +1,22 @@
 import json
 import argparse
-from orm import UrlData
+import logging
+
+from queries import UrlData
 from url_sentry import URL
+
+
+# Configure the logging
+logging.basicConfig(
+    level=logging.WARNING,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+# Create a logger for the CLI
+cli_logger = logging.getLogger('cli_logger')
+cli_logger.setLevel(logging.INFO)
+console_handler = logging.StreamHandler()
+cli_logger.addHandler(console_handler)
 
 
 parser = argparse.ArgumentParser(description='URL Data Management')
@@ -13,28 +28,52 @@ parser.add_argument('-js', action='store_true', help='Include JS')
 parser.add_argument('-subs', action='store_true', help='Include all records')
 parser.add_argument('-content-length', action='store_true', help='Include content length')
 parser.add_argument('-body', action='store_true', help='Content of the body')
-
+parser.add_argument('-logs', action='store_true', help='Show logs')
 
 args = parser.parse_args()
 
+if args.logs:
+    cli_logger.setLevel(logging.WARNING)
+    log_file_handler = logging.FileHandler('log.txt')
+    cli_logger.addHandler(log_file_handler)
+
 if args.action == 'add':
-    url_data = UrlData(url=args.url)
-    domain = URL(args.url)
 
-    if args.status_code:
-        url_data.status_code = domain.check_status_code()
+    url_data = UrlData.get(args.url)
+    if url_data:
+        if args.status_code:
+            url_data.status_code = URL(args.url).check_status_code()
 
-    if args.title:
-        url_data.title = domain.check_title()
+        if args.title:
+            url_data.title = URL(args.url).check_title()
 
-    if args.js:
-        url_data.js_file = domain.check_js_files() 
-    
-    if args.content_length:
-        url_data.content_length = domain.check_content_length()
+        if args.js:
+            url_data.js_hash = URL(args.url).check_js_files()
 
-    url_data.save()
-    print('Data added successfully!')
+        if args.content_length:
+            url_data.content_length = URL(args.url).check_content_length()
+
+        url_data.update()
+        print('Data updated successfully!')
+    else:
+        url_data = UrlData(url=args.url)
+        domain = URL(args.url)
+
+        if args.status_code:
+            url_data.status_code = domain.check_status_code()
+
+        if args.title:
+            url_data.title = domain.check_title()
+
+        if args.js:
+            url_data.js_hash = domain.check_js_files()
+            print(domain.check_js_files())
+
+        if args.content_length:
+            url_data.content_length = domain.check_content_length()
+
+        url_data.save()
+        print('Data added successfully!')
 
 elif args.action == 'remove':
     url_data = UrlData.get(args.url)
