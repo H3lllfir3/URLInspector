e19e972eef25a8b933a531825ed7de2e3d5accb2
H3lllfir3 <gholami.ayub73+github@gmail.com>
2023-07-23 16:34:51 +0200

    using python cron job module for scheduling.

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
+import os
 from setuptools import setup
+from setuptools.command.install import install
+from crontab import CronTab
+
+class CustomInstallCommand(install):
+    def run(self):
+        # Call the original install command first
+        install.run(self)
+
+        # Define the cronjob details
+        script_path = os.path.join(os.path.abspath("main.py"))
+        interval_minutes = 5  # Replace with the desired interval in minutes (1 hour in this case)
+
+        # Add the cronjob
+        cron = CronTab(user=True)
+        job = cron.new(command=f'python3 {script_path}')
+        job.minute.every(interval_minutes)
+        cron.write()
 
 setup(
     name='URL_sentry',
@@ -7,12 +25,14 @@ setup(
     install_requires=[
         'rich',
         'requests',
-        'python-decouple',
-        'python-crontab',
+        'python-decouple'
     ],
     entry_points={
         'console_scripts': [
             'url-sentry=cli.cli:main',
         ],
     },
+    cmdclass={
+        'install': CustomInstallCommand,
+    }
 )
