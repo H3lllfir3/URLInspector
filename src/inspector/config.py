be945ec182e3fe55ccaf1333746fb60ad068a334
H3lllfir3 <gholami.ayub73+github@gmail.com>
2023-10-11 22:51:18 +0200

    using pathlib instead of the os for...

--- src/inspector/config.py
+++ src/inspector/config.py
@@ -1,1 +1,26 @@
-DB_URL = 'sqlite:///data.db'
+import logging
+from pathlib import Path
+
+
+HOME = Path.home()
+BASE_DIR = HOME / '.inspector'
+DB_URL = f'sqlite:///{BASE_DIR}/data.db'
+LOG_FILE = BASE_DIR / 'app.log'
+
+if not BASE_DIR.exists():
+    BASE_DIR.mkdir()
+
+
+def get_logger():
+
+    logger = logging.getLogger('myapp')
+    logger.setLevel(logging.INFO)
+
+    file_handler = logging.FileHandler(LOG_FILE)
+    file_handler.setLevel(logging.INFO)
+    file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(file_formatter)
+
+    logger.addHandler(file_handler)
+
+    return logger
