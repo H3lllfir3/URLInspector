5691b5cc1a14c89bef0c29b8fe6e4c54cf73fb8f
H3lllfir3 <gholami.ayub73+github@gmail.com>
2023-10-25 23:52:27 +0200

    Improve DiscordNotification class

--- src/inspector/discord_client.py
+++ src/inspector/discord_client.py
@@ -1,79 +1,28 @@
-import json
-from datetime import datetime
-
-import requests
-
-from .config import get_logger
-
-logger = get_logger()
+from discord_webhook import DiscordEmbed
+from discord_webhook import DiscordWebhook
 
 
-class DiscordMessage:
-    """Class to generate a Discord message payload."""
-
-    def __init__(self, title: str, url: str) -> None:
-        """Initialize a DiscordMessage.
-
-        Args:
-            title (str): The title for the message embed.
-            url (str): The url for the message embed.
-        """
-        self.title = title
-        self.url = url
-
-    def to_dict(self) -> dict:
-        """Convert the message to a dictionary payload.
-
-        Returns:
-            Dict: The dictionary payload
-        """
-
-        return {
-            'content': f':x:   **{self.title}**!',
-            'embeds': [
-                {
-                    'title': self.url,
-                    'color': 'null',
-                    'timestamp': datetime.now().strftime('%Y%m%d-%H%M%S'),
-                },
-            ],
-            'username': 'UrlSentry :eye:',
-            'avatar_url': 'https://github.com/zi-gax/Hunt/assets/67065043/e844e723-79c9-4913-b101-7a59d8d3eabe',
-            'attachments': [],
-        }
-
-
-class DiscordWebhook:
-    """Class to send messages to a Discord webhook."""
+class DiscordNotification:
 
     def __init__(self, webhook_url: str) -> None:
-        """Initialize the webhook.
-
-        Args:
-            webhook_url (str): The Discord webhook URL.
-        """
-        self.webhook_url = webhook_url
-        self.headers = {
-            'Content-Type': 'application/json',
+        self.webhook = DiscordWebhook(url=webhook_url, username='URL Inspector')
+        self.embed = DiscordEmbed
+        self.STATUS = {
+            'red': ':red_circle:',
+            'green': ':green_circle:',
+            'blue': ':blue_circle:',
         }
 
-    def send_message(self, title: str, url: str) -> None:
+    def send_message(self, title: str, url: str, status: str) -> None:
         """Send a message to the webhook.
 
         Args:
             title (str): The title for the Discord message.
             url (str): The url for the Discord message.
         """
-        message = DiscordMessage(title, url)
-        payload = message.to_dict()
+        embed = self.embed(title=f'{self.STATUS[status]}   {title}', description=f'{url}', color='03b2f8')
+        embed.set_author(name='URL Inspector', url='https://github.com/H3lllfir3', icon_url='https://avatars.githubusercontent.com/u/55285134')
+        embed.set_timestamp()
 
-        response = requests.post(
-            self.webhook_url,
-            data=json.dumps(payload),
-            headers=self.headers,
-        )
-
-        if response.status_code == 204:
-            logger.info('Message sent successfully.')
-        else:
-            logger.error(f'Failed to send message. Status code: {response.status_code}')
+        self.webhook.add_embed(embed)
+        self.webhook.execute()
